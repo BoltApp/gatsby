3a5d0512380fafe6e29e0b1f6dc964c998b0da81
Long Yao <long@bolt.com>
2022-12-29 15:17:06 -0500

    Add fake checkout everywhere links

--- examples/creating-source-plugins/gatsby-starter-default/src/pages/index.js
+++ examples/creating-source-plugins/gatsby-starter-default/src/pages/index.js
@@ -44,9 +44,11 @@ export default ({ data }) => (
             <img src={media.url} alt={media.url}></img>
           ))}
           <p className={styles.listItemDescription}>{product.description}</p>
-          <div data-tid="instant-bolt-checkout-button">
-            <object data={`${buttonUrlBase}/v1/checkout_button?publishable_key=${publishableKey}`} />
-          </div>
+          <a href={`${buttonUrlBase}/product_checkout.html?merchant_division_id=AHX6aVWANY_0&publisher_key=06af3ae7a5145505922be60482c5a71e&bolt_product_id=86396606-52e6-5743-a03f-1200cdd8b8c1`}>
+            <div data-tid="instant-bolt-checkout-button">
+              <object data={`${buttonUrlBase}/v1/checkout_button?publishable_key=${publishableKey}`} />
+            </div>
+          </a>
         </li>
       ))}
     </ul>
